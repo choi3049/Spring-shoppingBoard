ad1e6cc2be70335c19a21bbb9e67adfde67650e6
CHOE BYEONGSEON <qudtjs3012@naver.com>
2022-03-18 16:49:35 +0900

    Main Page, Main home, Sign up and log in

---
+++ sun/src/main/java/com/sun/persistence/MemberDAO.java
@@ -0,0 +1,12 @@
+package com.sun.persistence;
+
+import com.sun.domain.MemberVO;
+
+public interface MemberDAO {
+
+	// 회원 가입
+	public void signup(MemberVO vo) throws Exception;
+
+	// 로그인
+	public MemberVO signin(MemberVO vo) throws Exception;
+}
